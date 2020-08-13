11e1f7cf0fe07d0ae6f65d06cca98e2308d1140d
jose MARIN <josermarinr18@gmail.com>
2020-08-13 21:11:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var HunterSDK = require('hunterio');
 
-var KEY = '447d75269d37c8d96999b289ef37c8940de4d235';
+var KEY = 'xwxaxaxazaeazasazeartfadfarrfarfafz';
 
 var hunter = new HunterSDK(KEY);
 
 let mails = [];
 
 hunter.domainSearch({
-    domain: 'https://www.aktarma.fr/'
+    domain: 'https://www.siteweb.fr/'
 }, function(err, body) {
     if (err) {
         console.log(err)
